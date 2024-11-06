f073c68407a722e5bc7bf2bc2928e37b947978a9
A1exdumb <polonchip123@gmail.com>
2024-11-06 07:03:18 +0000

    finished ex5

--- ex5/main.c
+++ ex5/main.c
@@ -3,5 +3,11 @@
 
 int main(void)
 {
-    printf("Hello World\n");
+    char small_letter, capital_letter;
+    for (int i = 0; i < 26; i++) {
+        small_letter = 'a' + i; 
+        capital_letter = 'A' + i; 
+
+        printf("%c\t\t%d\t\t%c\t\t%d\n", small_letter, small_letter, capital_letter, capital_letter);
+    }
 }
